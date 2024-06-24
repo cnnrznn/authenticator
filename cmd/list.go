85a2e14187a52ac456ab0435f5133604143dc069
Connor Zanin <cnnrznn@gmail.com>
2024-06-24 09:46:04 -0600

    List implementation

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/cnnrznn/authenticator/store"
+	"github.com/cnnrznn/authenticator/totp"
 	"github.com/spf13/cobra"
 )
 
@@ -9,10 +14,20 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists apps and current tokens",
 
-	Run: func(cmd *cobra.Command, args []string) {
-		// Read apps from storage
-		// Compute current keys
-		// Print
+	RunE: func(cmd *cobra.Command, args []string) error {
+		tokens, _ := store.Load()
+
+		fmt.Printf("%16v %-16v\n", "Name", "Token")
+		for _, t := range tokens {
+			token, err := totp.Generate(t.Secret, time.Now())
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("%16v %-16v\n", t.Name, token)
+		}
+
+		return nil
 	},
 }
 
