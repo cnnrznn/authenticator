f9394ac458a6b9721bb34cb7b0d8a01b52a6bfa4
Connor Zanin <cnnrznn@gmail.com>
2024-06-25 15:10:05 -0600

    Fix tests

--- totp/totp_test.go
+++ totp/totp_test.go
@@ -8,39 +8,43 @@ import (
 )
 
 func TestStep(t *testing.T) {
+	step, _ := calcStep(time.Unix(90, 0))
 	assert.Equal(t,
 		uint64(3),
-		step(time.Unix(90, 0)),
+		step,
 	)
 
+	step, _ = calcStep(time.Unix(91, 0))
 	assert.Equal(t,
 		uint64(3),
-		step(time.Unix(91, 0)),
+		step,
 	)
 
+	step, _ = calcStep(time.Unix(119, 0))
 	assert.Equal(t,
 		uint64(3),
-		step(time.Unix(119, 0)),
+		step,
 	)
 
+	step, _ = calcStep(time.Unix(120, 0))
 	assert.Equal(t,
 		uint64(4),
-		step(time.Unix(120, 0)),
+		step,
 	)
 }
 
 func FuzzStep(f *testing.F) {
 	f.Fuzz(func(t *testing.T, i int64) {
 		tx := time.Unix(i, 0)
-		s := step(tx)
+		s, _ := calcStep(tx)
 		expected := uint64(i / 30)
 		assert.Equal(t, expected, s)
 	})
 }
 
 func TestTOTP(t *testing.T) {
-	s, err := Generate("JBSWY3DPEHPK3PXP", time.Unix(0, 0))
+	s, _, err := Generate("JBSWY3DPEHPK3PXP", time.Unix(0, 0))
 
 	assert.NoError(t, err)
-	assert.Equal(t, "282760", s)
+	assert.Equal(t, "282-760", s)
 }
